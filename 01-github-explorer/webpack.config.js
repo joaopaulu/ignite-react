ee78ccec55528d107cd2914232069eabd89d231d
João Paulo <jptick@gmail.com>
2021-03-09 20:37:19 -0300

    Update Settings Webpack

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,22 +1,39 @@
-const path = require('path')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
-  output:{
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
-  resolve:{
-    extensions: ['.js', '.jsx'],
+  resolve: {
+    extensions: [".js", ".jsx"],
   },
-  module:{
-    rules:[
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
       {
-        test:/\.jsx$/,
+        test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
-      }
+        use: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
-  }
+  },
 };
