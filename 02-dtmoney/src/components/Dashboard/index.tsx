ad13411d19c3ce888db086e1e4f1c2733cd74038
joaopaulu <jptick@gmail.com>
2021-03-22 18:51:44 -0300

    Create TransactionTable Component

--- 02-dtmoney/src/components/Dashboard/index.tsx
+++ 02-dtmoney/src/components/Dashboard/index.tsx
@@ -1,10 +1,13 @@
-import { Summary } from '../Summary';
+import { Summary } from 'components/Summary';
+import { TransactionTable } from '../TransactionTable';
+
 import { Container } from './styles';
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransactionTable />
     </Container>
   );
 }
