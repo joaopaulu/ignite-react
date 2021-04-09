9315f0b493a787bc208ed473315b86d82a06e6c9
João Paulo <jptick@gmail.com>
2021-04-09 10:54:06 -0300

    Update Component TransactionTable

--- 02-dtmoney/src/components/TransactionTable/index.tsx
+++ 02-dtmoney/src/components/TransactionTable/index.tsx
@@ -1,10 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { api } from 'services/api';
 import { Container } from './styles';
 
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
   useEffect(() => {
-    api.get('/api/transactions').then(response => console.log(response.data));
+    api
+      .get('/api/transactions')
+      .then(response => setTransactions(response.data.transactions));
   }, []);
 
   return (
@@ -20,18 +33,23 @@ export function TransactionTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de Site</td>
-            <td className="deposit">R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">-R$2.000</td>
-            <td>Casa</td>
-            <td>01/03/2021</td>
-          </tr>
+          {transactions.map(transactions => (
+            <tr key={transactions.id}>
+              <td>{transactions.title}</td>
+              <td className={transactions.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                }).format(transactions.amount)}
+              </td>
+              <td>{transactions.category}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(transactions.createdAt),
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
