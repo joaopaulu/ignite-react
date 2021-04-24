46dbbdc195848b695fd0a8b94208d782ba91e1a0
João Paulo <jptick@gmail.com>
2021-04-24 11:18:53 -0300

    Create Header Component

--- 03-ignews/src/pages/_app.tsx
+++ 03-ignews/src/pages/_app.tsx
@@ -1,9 +1,15 @@
+import { Header } from 'components/Header';
 import { AppProps } from 'next/app';
 
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
